3108f87dd2445743dacdff072f706a91802d3814
dp3735 <dp3735@nyu.edu>
2023-10-18 20:35:03 +0800

    set up a new project for Project A

--- project-template/js/index.js
+++ project-template/js/index.js
@@ -1,7 +1,7 @@
-function setup(){
-    createCanvas(400,400);
+function setup() {
+    createCanvas(400, 400);
 }
 
-function draw (){
-    background (255,255,0);
+function draw() {
+    background(255, 255, 0);
 }
